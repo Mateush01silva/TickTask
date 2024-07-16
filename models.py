909a9c563fb02fabeff51dd69980eb64525d69bb
Mateush01silva <silva.mateush01@gmail.com>
2024-07-16 20:04:58 -0300

    mais alteracoes

--- models.py
+++ models.py
@@ -14,21 +14,22 @@ class TaskTickModel:
         df = pd.read_excel(self.db_path, sheet_name="Atividades")
         return df['Projeto'].tolist()
     
-    def cadastrar_atividade(self, projeto, descricao, cliente):
-        nova_atividade = pd.DataFrame([[projeto, descricao, cliente]], columns=['Projeto', 'Descrição', 'Cliente'])
-        try:
-            if os.path.exists(self.db_path):
-                book = load_workbook(self.db_path)
-                with pd.ExcelWriter(self.db_path, engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
-                    writer.book = book
-                    writer.sheets = {ws.title: ws for ws in book.worksheets}
-                    startrow = writer.sheets['Atividades'].max_row
-                    nova_atividade.to_excel(writer, sheet_name='Atividades', index=False, header=False, startrow=startrow)
-            else:
-                with pd.ExcelWriter(self.db_path, engine='openpyxl') as writer:
-                    nova_atividade.to_excel(writer, index=False, header=True, sheet_name='Atividades')
-        except PermissionError:
-            raise PermissionError("O arquivo já está aberto. Por favor, feche o arquivo e tente novamente.")
+def cadastrar_atividade(self, projeto, descricao, cliente):
+    nova_atividade = pd.DataFrame([[projeto, descricao, cliente]], columns=['Projeto', 'Descrição', 'Cliente'])
+    try:
+        if os.path.exists(self.db_path):
+            book = load_workbook(self.db_path)
+            with pd.ExcelWriter(self.db_path, engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
+                writer.sheets = {ws.title: ws for ws in book.worksheets}
+                startrow = writer.sheets['Atividades'].max_row
+                nova_atividade.to_excel(writer, sheet_name='Atividades', index=False, header=False, startrow=startrow)
+                writer._save()
+        else:
+            with pd.ExcelWriter(self.db_path, engine='openpyxl') as writer:
+                nova_atividade.to_excel(writer, index=False, header=True, sheet_name='Atividades')
+    except PermissionError:
+        raise PermissionError("O arquivo já está aberto. Por favor, feche o arquivo e tente novamente.")
+
 
 #    def cadastrar_atividade(self, projeto, descricao, cliente):
 #        nova_atividade = pd.DataFrame([[projeto, descricao, cliente]], columns=['Projeto', 'Descrição', 'Cliente'])
